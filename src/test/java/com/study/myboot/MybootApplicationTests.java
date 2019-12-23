d0f3636f747ef358d881a20e14fdfdcae7cd550e
yulongshuai878 <41602302+yulongshuai878@users.noreply.github.com>
2019-12-23 11:33:09 +0800

    init project

---
+++ src/test/java/com/study/myboot/MybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.myboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
